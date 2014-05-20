83b4a6c8f1fd61ee0e290b637e92747a55cb0cc8
jondalley <jondalley@hotmail.com>
2014-05-20 15:01:57 +0100

    Coursera - R programming - Assignment 2
    My solution to programming assignment 2 on the Coursera; Introduction
    to R programming course - delivered by John Hopkins University

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## make CacheMatrix is an edit of makeVector function provided
+## `makeVector` created a special "vector", 
+## which was really a list containing a function to
+## 1.  set the value of the vector
+## 2.  get the value of the vector
+## 3.  set the value of the mean
+## 4.  get the value of the mean
 
-## Write a short comment describing this function
+## the function now works in a similar way for 1 and 2
+## 3 and 4 now respectively set and get the value of the 
+## solved/inverted matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(s) {
+                x <<- s
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) s <<- solve
+        getinv <- function() s
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## edit of cachemean function provided
+## This function checks whether the inverse of the output of
+## makeCacheMatrix has previously been cached. If found then 
+##it returns the result with the comment that it is getting  
+## cache data.
+## If not then it calculates the inverse of the output of 
+## makeCacheMatrix, stores it in cache, and displays the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getinv()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setinv(s)
+        s
 }
